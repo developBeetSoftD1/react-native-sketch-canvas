934dca14ed3e1f8b5a097415e334998bf89b2a4f
developBeetSoftD1 <develop.d1@beetsoft.com.vn>
2023-11-07 12:00:10 +0700

    Update RNSketchCanvas.m

--- ios/RNSketchCanvas/RNSketchCanvas/RNSketchCanvas.m
+++ ios/RNSketchCanvas/RNSketchCanvas/RNSketchCanvas.m
@@ -304,7 +304,8 @@
         CGContextRef context = UIGraphicsGetCurrentContext();
         if (!transparent) {
             // CGContextSetRGBFillColor(context, 0,0,1.0, 1); // hashcode from smdc
-            CGContextSetRGBFillColor(context, 0.0f, 0.0f, 0.0f, 1.0f); 
+            CGContextSetRGBFillColor(context, 1.0f, 1.0f, 1.0f, 1.0f);
+            // CGContextSetFillColorWithColor(context,  [UIColor whiteColor].CGColor) 
             CGContextFillRect(context, rect);
         }
         CGRect targetRect = [Utility fillImageWithSize:self.bounds.size toSize:rect.size contentMode:@"AspectFill"];
@@ -337,7 +338,7 @@
         CGContextRef context = UIGraphicsGetCurrentContext();
         if (!transparent) {
             // CGContextSetRGBFillColor(context, 0,0,1.0, 1); // hashcode from smdc
-            CGContextSetRGBFillColor(context, 0.0f, 0.0f, 0.0f, 1.0f); 
+            CGContextSetRGBFillColor(context, 1.0f, 1.0f, 1.0f, 1.0f);
             CGContextFillRect(context, rect);
         }
         if (_backgroundImage && includeImage) {
